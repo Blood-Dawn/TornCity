54ee11c887dc51177a78ac59c8f3b52e6af4c8b1
Blood-Dawn <151286372+Blood-Dawn@users.noreply.github.com>
2026-02-11 21:24:57 -0500

    clean up

--- python-text_env.py
+++
@@ -1,9 +0,0 @@
-from dotenv import load_dotenv
-import os
-from pathlib import Path
-
-# Specify the full path to the .env file
-env_path = Path('.') / '.env'
-load_dotenv(dotenv_path=env_path)
-
-print("DISCORD_TOKEN:", os.getenv("DISCORD_TOKEN"))
